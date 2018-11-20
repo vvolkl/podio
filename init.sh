c6edb9cd4c162ee1aa32e74662faeeb1b04389be
Valentin Volkl <valentin.volkl@cern.ch>
2018-11-20 14:28:08 +0100

    Remove os detection (we have no externals apart from slc6)

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
-OS=`python $TOOLSPATH/hsf_get_platform.py --get os`
 
 # source FCC externals
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.0.0/x86_64-$OS-gcc62-opt/setup.sh
+source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.0.0/x86_64-slc6-gcc62-opt/setup.sh
 
 # Define path to podio
 # CMakeLists.txt relies on some environment variables
