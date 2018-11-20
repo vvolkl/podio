49fbdd8db325a4aed15a3c2a1b7a57abf8f3fb33
Valentin Volkl <valentin.volkl@cern.ch>
2018-11-20 14:35:24 +0100

    Reset podio env var

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.0.0/x86_64-slc6-gcc62-o
 
 # Define path to podio
 # CMakeLists.txt relies on some environment variables
-export PODIO=$PWD
+export PODIO=$FCCVIEW
